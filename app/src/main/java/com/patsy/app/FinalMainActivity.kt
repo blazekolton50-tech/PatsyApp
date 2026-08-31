06b549852ebc48707b434c9a37dc0f0a5c6602cc
blazekolton50-tech <blazekolton50@gmail.com>
2026-08-31 01:36:59 +0100

    fix: resolve Compose weight in launcher scope

--- app/src/main/java/com/patsy/app/FinalMainActivity.kt
+++ app/src/main/java/com/patsy/app/FinalMainActivity.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.weight
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.darkColorScheme
