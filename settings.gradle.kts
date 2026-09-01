911e2cfc75155ecafcb4c79e495a013cce46cc11
blazekolton50-tech <blazekolton50@gmail.com>
2026-09-01 22:26:22 +0100

    Rename Gradle project to THyNK-IN

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,4 @@
 pluginManagement { repositories { google(); mavenCentral(); gradlePluginPortal() } }
 dependencyResolutionManagement { repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS); repositories { google(); mavenCentral() } }
-rootProject.name = "patsy1"
+rootProject.name = "THyNK-IN"
 include(":app")
